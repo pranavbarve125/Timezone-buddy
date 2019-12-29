91ea91b6bd37bec036b588f3cfea632141c29968
Pranav Barve <pranavbarve98@google.com>
2019-12-29 23:57:46 +0530

    Basic structure

---
+++ singleguy.py
@@ -0,0 +1,7 @@
+class dude:
+    def __init__(self, inputperiods, timezone, nativezonetime):
+        self.inputperiods = inputperiods
+        self.timezone = timezone
+        self.nativezonetime = nativezonetime
+
+    
---
+++ tmconvertor.py
@@ -0,0 +1,14 @@
+#process before taking inputs from all the users.
+# step 0: enter a particular set of favourable 24 hours. Those 24 hours will be shown to all the users in native time zones. 
+
+# inputs from various users
+# step1 : number of freezones
+# step2 : taking input for the freezones
+
+# trying to find common time timethrough GMT + 0
+# step3 : converting all the data to GMT
+# step4 : look for clashes
+# step5 : convert all the clashes back to native timezones
+
+# display the common time periods for all the time zones respectively
+# step6 : display the converted zones to respective users 
