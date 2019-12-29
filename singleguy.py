602cf1cce8a7134bd61f29b246ddb51bac53ee7b
Pranav Barve <pranavbarve98@google.com>
2019-12-30 02:12:01 +0530

    Kickstart

--- singleguy.py
+++ singleguy.py
@@ -1,7 +1,14 @@
 class dude:
-    def __init__(self, inputperiods, timezone, nativezonetime):
+    def __init__(self, inputperiods=None, timezone=None, nativezonetime=None):
         self.inputperiods = inputperiods
         self.timezone = timezone
         self.nativezonetime = nativezonetime
 
+    def __init__(self):
+        return
+
+    def set(self, inputperiods=None, timezone=None, nativezonetime=None):
+        self.inputperiods = inputperiods
+        self.timezone = timezone
+        self.nativezonetime = nativezonetime
     
--- tmconvertor.py
+++ tmconvertor.py
@@ -1,9 +1,15 @@
+from singleguy import dude
+import datetime
+import pytz
 #process before taking inputs from all the users.
 # step 0: enter a particular set of favourable 24 hours. Those 24 hours will be shown to all the users in native time zones. 
 
 # inputs from various users
 # step1 : number of freezones
 # step2 : taking input for the freezones
+User1 = dude()
+User2 = dude()
+
 
 # trying to find common time timethrough GMT + 0
 # step3 : converting all the data to GMT
