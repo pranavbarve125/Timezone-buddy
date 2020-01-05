591c3b5dcdf9a43322da71d907d40a4b2d84b73c
Pranav Barve <pranavbarve98@google.com>
2020-01-06 02:01:47 +0530

    Input number size changed

--- webapp.py
+++ webapp.py
@@ -8,9 +8,9 @@ app.config["SECRET_KEY"] = "1737e4b44bd8ec494db26e5724b6e110"
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        return "Works"
+        return str(request.form)
     else:
-        return render_template("index.html", users=4, timeslots=4, timezones=data.timezone_names)
+        return render_template("singleuser.html", users=4, timeslots=4, timezones=data.timezone_names)
 
 if __name__ == '__main__':
     app.run(debug=True)
